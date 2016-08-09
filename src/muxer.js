ed917821269c39085ff72809895fc6956a3f21c1
dignifiedquire <dignifiedquire@gmail.com>
2016-08-09 13:01:55 +0200

    first phase pull

---
+++ src/muxer.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const EventEmitter = require('events').EventEmitter
+const noop = require('lodash.noop')
+const Connection = require('interface-connection').Connection
+const toPull = require('stream-to-pull-stream')
+
+const SPDY_CODEC = require('./spdy-codec')
+
+module.exports = class Muxer extends EventEmitter {
+  constructor (conn, spdy) {
+    super()
+
+    this.spdy = spdy
+    this.conn = conn
+    this.multicodec = SPDY_CODEC
+
+    spdy.start(3.1)
+
+    // The rest of the API comes by default with SPDY
+    spdy.on('close', () => {
+      this.emit('close')
+    })
+
+    spdy.on('error', (err) => {
+      this.emit('error', err)
+    })
+
+    // needed by other spdy impl that need the response headers
+    // in order to confirm the stream can be open
+    spdy.on('stream', (stream) => {
+      stream.respond(200, {})
+      const muxedConn = new Connection(toPull.duplex(stream))
+      proxyMethods(conn, muxedConn)
+      this.emit('stream', muxedConn)
+    })
+  }
+
+  // method added to enable pure stream muxer feeling
+  newStream (callback) {
+    if (!callback) {
+      callback = noop
+    }
+    const request = this.spdy.request({
+      method: 'POST',
+      path: '/',
+      headers: {}
+    }, callback)
+    const muxedConn = new Connection(toPull.duplex(request))
+
+    proxyMethods(this.conn, muxedConn)
+    return muxedConn
+  }
+
+  end (cb) {
+    this.spdy.end(cb)
+  }
+}
+
+function proxyMethods (src, target) {
+  if (src.getObservedAddrs) {
+    target.getObservedAddrs = src.getObservedAddrs.bind(src)
+    target.getPeerInfo = src.getPeerInfo.bind(src)
+    target.setPeerInfo = src.setPeerInfo.bind(src)
+  }
+}
